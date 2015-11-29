17dfb4196dab7931d1faec137da34210b99b32dd
Maicon Henrique <maiconhenriquekcond@gmail.com>
2015-11-29 02:10:44 +0000

    Cadastro de Unidade de medidas finalizado 100%

--- app/controllers/unid_meds_controller.rb
+++ app/controllers/unid_meds_controller.rb
@@ -2,76 +2,68 @@ class UnidMedsController < ApplicationController
   before_action :set_unid_med, only: [:show, :edit, :update, :destroy]
   before_action :authenticate_user!
   
-  # GET /unid_meds
-  # GET /unid_meds.json
   def index
-    @unid_meds = UnidMed.all
-    authorize @unid_meds
+    @q = UnidMed.ransack(params[:q])
+    @unid_meds = @q.result
+    
   end
 
-  # GET /unid_meds/1
-  # GET /unid_meds/1.json
   def show
   end
 
-  # GET /unid_meds/new
   def new
     @unid_med = UnidMed.new
-    authorize @unid_med
   end
 
-  # GET /unid_meds/1/edit
-  def edit
-  end
-
-  # POST /unid_meds
-  # POST /unid_meds.json
   def create
-    @unid_med = UnidMed.new(unid_med_params)
+    @unid_med = UnidMed.create(unid_med_params)
 
     respond_to do |format|
       if @unid_med.save
-        format.html { redirect_to @unid_med, notice: 'Unid med was successfully created.' }
-        format.json { render :show, status: :created, location: @unid_med }
+        format.json { head :no_content }
+        format.js
       else
-        format.html { render :new }
-        format.json { render json: @unid_med.errors, status: :unprocessable_entity }
+        format.json { render json: @unid_med.errors.full_messages, 
+                            status: :unprocessable_entity }
       end
+      
     end
   end
 
-  # PATCH/PUT /unid_meds/1
-  # PATCH/PUT /unid_meds/1.json
+  def edit
+  end
+
   def update
-    respond_to do |format|
+     respond_to do |format|
       if @unid_med.update(unid_med_params)
-        format.html { redirect_to @unid_med, notice: 'Unid med was successfully updated.' }
-        format.json { render :show, status: :ok, location: @unid_med }
+        format.json { head :no_content }
+        format.js
       else
-        format.html { render :edit }
-        format.json { render json: @unid_med.errors, status: :unprocessable_entity }
+        format.json { render json: @unid_med.errors.full_messages,
+                                   status: :unprocessable_entity }
       end
+     
     end
   end
 
-  # DELETE /unid_meds/1
-  # DELETE /unid_meds/1.json
+
   def destroy
+
     @unid_med.destroy
     respond_to do |format|
-      format.html { redirect_to unid_meds_url, notice: 'Unid med was successfully destroyed.' }
+      format.js
+      format.html { redirect_to posts_url }
       format.json { head :no_content }
     end
   end
 
-  private
-    # Use callbacks to share common setup or constraints between actions.
-    def set_unid_med
-      @unid_med = UnidMed.find(params[:id])
-    end
-
-    # Never trust parameters from the scary internet, only allow the white list through.
-    def unid_med_params
-      params.require(:unid_med).permit(:descricao, :abreviacao)
-    end
+private
+  
+  def set_unid_med
+    @unid_med = UnidMed.find(params[:id])
+  end
+  
+  def unid_med_params
+    params.require(:unid_med).permit(:descricao, :abreviacao)
+  end
 end
--- app/models/unid_med.rb
+++ app/models/unid_med.rb
@@ -1,2 +1,12 @@
 class UnidMed < ActiveRecord::Base
+
+	validates :descricao, :abreviacao, presence: true
+	validates :descricao, :abreviacao, uniqueness: true
+
+	before_validation :uppercase_campo
+
+  def uppercase_campo
+    self.descricao.upcase!
+    self.abreviacao.upcase!
+  end
 end
